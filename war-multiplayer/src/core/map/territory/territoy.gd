a1a076be491b7e0826a8578bee5cb1b25a779aaf
Whesley Felipe <111021322+X4vierWhes@users.noreply.github.com>
2026-06-13 15:54:52 -0300

    wip: basic territory

--- war-multiplayer/src/core/map/territory/territoy.gd
+++ war-multiplayer/src/core/map/territory/territoy.gd
@@ -5,7 +5,7 @@ class_name Territory
 
 
 @export_category("Parameters")
-@export var territory_texture: TextureRect
+@export var texture: TextureRect
 @export var connected_regions: Array[Territory] = []
 @export var territory_name: String
 @export var army_number: int = 1
@@ -16,13 +16,20 @@ const label_text: String = "[wave freq=5.0 amp=50.0 connected=0] {text} [/wave]"
 
 func _ready() -> void:
 	_change_color(Color.RED)
-	_change_army_count(55)
+	_change_army_count(army_number + 120)
 
 func _change_army_count(amount: int) -> void:
 	rich_text_label.text = label_text.replace("{text}", str(amount))
 
 func _change_color(new_color: Color) -> void:
-	print("entrei")
-	var shader_material = territory_texture.material as ShaderMaterial
+	var shader_material = texture.material as ShaderMaterial
 	if shader_material:
 		shader_material.set_shader_parameter("new_color", new_color)
+
+
+func _on_texture_rect_mouse_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_texture_rect_mouse_exited() -> void:
+	pass # Replace with function body.
