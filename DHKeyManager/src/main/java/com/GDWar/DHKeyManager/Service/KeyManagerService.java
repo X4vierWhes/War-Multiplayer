1b366f8f0cf851c40a647cc3e396d08f93309520
João Lucas <joao.duarte31219@alunos.ufersa.edu.br>
2026-06-18 19:07:44 -0300

    feat: serviço de gerenciamento de chaves

---
+++ DHKeyManager/src/main/java/com/GDWar/DHKeyManager/Service/KeyManagerService.java
@@ -0,0 +1,35 @@
+package com.GDWar.DHKeyManager.Service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class KeyManagerService {
+    ConcurrentHashMap<String, String> keyStore = new ConcurrentHashMap<>();
+
+    public KeyManagerService(){
+        keyStore = new ConcurrentHashMap<>();
+    }
+
+    public void registerKey(String userId, String publicKey) {
+        if (userId.isEmpty() || publicKey.isEmpty()) {
+            throw new IllegalArgumentException("User ID and public key must not be empty");
+        }
+        keyStore.put(userId, publicKey);
+    }
+
+    public String getKey(String userId) {
+        if (userId.isEmpty()) {
+            throw new IllegalArgumentException("User ID must not be empty");
+        }
+        return keyStore.get(userId);
+    }
+
+    public void deleteKey(String userId) {
+        if (userId.isEmpty()) {
+            throw new IllegalArgumentException("User ID must not be empty");
+        }
+        keyStore.remove(userId);
+    }
+}
