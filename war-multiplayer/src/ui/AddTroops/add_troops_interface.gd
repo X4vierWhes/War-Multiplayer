4e1ba91aaedcb087943074f2b8c63e40624a3fbf
João Lucas <joao.duarte31219@alunos.ufersa.edu.br>
2026-06-18 10:32:59 -0300

    feat: interfaces de etapas

---
+++ war-multiplayer/src/ui/AddTroops/add_troops_interface.gd
@@ -0,0 +1,37 @@
+extends Control
+
+signal confirmar_acao(quant: int)
+signal cancelar_acao
+
+@onready var cancelar: Button = %cancelar
+@onready var botao_remover: Button = %BotaoRemover
+@onready var quantidade: Label = %quantidade
+@onready var botao_adicionar: Button = %BotaoAdicionar
+@onready var concluir: Button = %concluir
+
+var territorio_destino: Territory
+
+var max_quantidade:int
+
+var contador:int:
+	set(i):
+		contador = i
+		if quantidade:
+			quantidade.text = str(i)
+
+func _ready() -> void:
+	contador = 1
+	botao_adicionar.pressed.connect(_incrementar_contador)
+	botao_remover.pressed.connect(_decrementar_contador)
+	cancelar.pressed.connect(confirmar_acao.emit.bind(contador))
+	concluir.pressed.connect(cancelar_acao.emit)
+
+func _incrementar_contador() -> void:
+	if contador == max_quantidade: return
+	contador += 1
+	territorio_destino.army_number += 1
+
+func _decrementar_contador() -> void:
+	if contador == 1: return
+	contador -= 1
+	territorio_destino.army_number -= 1
---
+++ war-multiplayer/src/ui/DeclareAttack/declare_attack_interface.gd
@@ -0,0 +1,37 @@
+extends Control
+
+signal confirmar_acao(quant: int)
+signal cancelar_acao
+
+@onready var cancelar: Button = %cancelar
+@onready var botao_remover: Button = %BotaoRemover
+@onready var quantidade: Label = %quantidade
+@onready var botao_adicionar: Button = %BotaoAdicionar
+@onready var concluir: Button = %concluir
+
+var territorio_origem: Territory
+var territorio_destino: Territory
+
+var contador:int = 0:
+	set(i):
+		contador = i
+		if quantidade:
+			quantidade.text = str(i)
+
+func _ready() -> void:
+	botao_adicionar.pressed.connect(_incrementar_contador)
+	botao_remover.pressed.connect(_decrementar_contador)
+	cancelar.pressed.connect(cancelar_acao.emit)
+	cancelar.pressed.connect(confirmar_acao.emit.bind(contador))
+
+func _incrementar_contador() -> void:
+	if contador == territorio_origem.territorio.quant: return
+	contador += 1
+	territorio_origem.army_number -= 1
+	territorio_destino.army_number += 1
+
+func _decrementar_contador() -> void:
+	if contador == 1: return
+	contador -= 1
+	territorio_origem.army_number += 1
+	territorio_destino.army_number -= 1
